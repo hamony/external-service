34be761e75ad07cf17bf648775b2c50fee7488f6
Khoa Nguyen <khoa.zamioza@gmail.com>
2024-03-29 20:56:59 +0700

    Apply deploy and build image on local by Google build docker

---
+++ build-deploy-ks8.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+eval $(minikube docker-env)
+# Jib google docker build with minikube
+./mvnw com.google.cloud.tools:jib-maven-plugin:dockerBuild -Dimage=external-service:v0.1.0-alpha
+
+# Build deployment and service
+kubectl apply -f ./k8s
